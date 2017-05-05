602641be894bfab0b8630e259a730f536c75ca92
fconsoni <franco.consoni@gmail.com>
2017-05-05 18:03:55 -0300

    Nuevo importadorCSV, bien hecho esta vez
    Mucho mas lindo que antes

--- src/main/java/model/ImportadorCSV.java
+++ src/main/java/model/ImportadorCSV.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
 
 public class ImportadorCSV extends ImportadorDeCuentasDeEmpresas {
 
@@ -23,49 +25,28 @@ public class ImportadorCSV extends ImportadorDeCuentasDeEmpresas {
 	@Override
 	protected List<LineaEmpresa> levantarEmpresasDelArchivo(String path) {
 		CSVReader reader;
-		String[] nextLine;
 		List<LineaEmpresa> empresasObtenidas = new LinkedList<>();
 
 		try {
 			reader = new CSVReader(new FileReader(path));
-			while ((nextLine = reader.readNext()) != null) {
-				empresasObtenidas.add(this.armarEmpresaCon(nextLine));
-			}
+			ColumnPositionMappingStrategy<LineaEmpresa> strat = new ColumnPositionMappingStrategy<LineaEmpresa>();
+			String[] columns = new String[] {"nombre", "periodo", "cuenta", "valor"};
+			CsvToBean<LineaEmpresa> csv = new CsvToBean<LineaEmpresa>(); 
+			
+			strat.setType(LineaEmpresa.class); 
+			strat.setColumnMapping(columns);
+			
+			empresasObtenidas = csv.parse(strat, reader);
+			
 			reader.close();
 		} catch (FileNotFoundException exception) {
 			throw new ErrorImportacionException("No existe el archivo");
 		} catch (IOException exception) {
-			throw new ErrorImportacionException("Formato de archivo incorrecto");
+			throw new ErrorImportacionException("Error al leer el archivo");
 		} catch (ArrayIndexOutOfBoundsException exception) {
 			throw new ErrorImportacionException("Formato de archivo incorrecto");
 		}
 
 		return empresasObtenidas;
-	}
-
-	private LineaEmpresa armarEmpresaCon(String[] nextLine) {
-		LineaEmpresa empresaArmada = new LineaEmpresa();
-		this.setearNombreA(empresaArmada, nextLine);
-		this.setearPeriodo(empresaArmada, nextLine);
-		this.setearCuenta(empresaArmada, nextLine);
-		this.setearValorDeCuentaDe(empresaArmada, nextLine);
-
-		return empresaArmada;
-	}
-
-	private void setearNombreA(LineaEmpresa empresa, String[] nextLine) {
-		empresa.setNombre(nextLine[0]);
-	}
-
-	private void setearPeriodo(LineaEmpresa empresa, String[] nextLine) {
-		empresa.setPeriodo(nextLine[1]);
-	}
-
-	private void setearCuenta(LineaEmpresa empresa, String[] nextLine) {
-		empresa.setCuenta(nextLine[2]);
-	}
-
-	private void setearValorDeCuentaDe(LineaEmpresa empresa, String[] nextLine) {
-		empresa.setValor(nextLine[3]);
 	}
 }
