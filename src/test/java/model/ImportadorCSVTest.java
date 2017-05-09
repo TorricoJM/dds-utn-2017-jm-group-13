8cc4ce041a59b3ae2158ec67e703ffb232849e6e
Alejo Perrone <lejaperrone@gmail.com>
2017-05-09 13:35:48 -0300

    Agrego assert equals pero sigue sin funcionar como quisiera

--- src/test/java/model/ImportadorCSVTest.java
+++ src/test/java/model/ImportadorCSVTest.java
@@ -1,11 +1,10 @@
 package model;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class ImportadorCSVTest {
@@ -31,7 +30,7 @@ public ImportadorCSV importadorCSV;
 	e2.setNombre("sprite");
 	e2.setPeriodo("2016");
 	e2.setCuenta("fds");
-	e2.setValor("100");
+	e2.setValor("1000000");
 		
 	List<LineaEmpresa> empresasEsperadas = new LinkedList<>();
 	empresasEsperadas.add(e);
@@ -41,8 +40,21 @@ public ImportadorCSV importadorCSV;
 	importadorCSV = new ImportadorCSV("empresas.csv");
 	List<LineaEmpresa> empresasObtenidas = importadorCSV.obtenerEmpresas();
 	
-	//No se porque el .equals falla, hay una diferencia de formatos
-	assertTrue((empresasObtenidas.size() == empresasEsperadas.size()));
+	//No se porque si hago assertEquals(empresasObtenidas, empresasEsperadas) no funciona y comparando atributo a atrubuto si.
+	assertEquals(empresasObtenidas.get(0).getNombre(), empresasEsperadas.get(0).getNombre());
+	assertEquals(empresasObtenidas.get(0).getPeriodo(), empresasEsperadas.get(0).getPeriodo());
+	assertEquals(empresasObtenidas.get(0).getCuenta(), empresasEsperadas.get(0).getCuenta());
+	assertEquals(empresasObtenidas.get(0).getValor(), empresasEsperadas.get(0).getValor());
+	
+	assertEquals(empresasObtenidas.get(1).getNombre(), empresasEsperadas.get(1).getNombre());
+	assertEquals(empresasObtenidas.get(1).getPeriodo(), empresasEsperadas.get(1).getPeriodo());
+	assertEquals(empresasObtenidas.get(1).getCuenta(), empresasEsperadas.get(1).getCuenta());
+	assertEquals(empresasObtenidas.get(1).getValor(), empresasEsperadas.get(1).getValor());
+
+	assertEquals(empresasObtenidas.get(2).getNombre(), empresasEsperadas.get(2).getNombre());
+	assertEquals(empresasObtenidas.get(2).getPeriodo(), empresasEsperadas.get(2).getPeriodo());
+	assertEquals(empresasObtenidas.get(2).getCuenta(), empresasEsperadas.get(2).getCuenta());
+	assertEquals(empresasObtenidas.get(2).getValor(), empresasEsperadas.get(2).getValor());
 	
 	}
 			
