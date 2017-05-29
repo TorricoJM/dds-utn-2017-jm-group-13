2aa8fb010ea18b98304d90d9ff963e774b7e014a
avaromatias <avaromatias@gmail.com>
2017-05-28 22:07:47 -0300

    Cambios en el CrearIndicadorWindow

--- src/main/java/ui/windows/CrearIndicadorWindow.java
+++ src/main/java/ui/windows/CrearIndicadorWindow.java
@@ -7,8 +7,10 @@ import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.widgets.Selector;
+import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.SimpleWindow;
 import org.uqbar.arena.windows.WindowOwner;
+import org.uqbar.arena.widgets.NumericField;
 
 import model.CuentaYValor;
 import model.Indicador;
@@ -35,25 +37,44 @@ public class CrearIndicadorWindow extends SimpleWindow<CrearIndicadorViewModel> 
 		form.setWidth(10);
 		this.setTitle("Crear Indicador");
 
+		new Label(form).setText("     ");
+		
 		new Label(form).setText("Indicadores");
 		new Selector<Indicador>(form).allowNull(true);
+		
+		new Label(form).setText("       ");
 
 		new Label(form).setText("Cuentas");
 		new Selector<CuentaYValor>(form).allowNull(true);
 
+		Panel crearIndicador = new Panel(mainPanel);
+		crearIndicador.setLayout(new HorizontalLayout());
+		
+		new Label(crearIndicador).setText("Nombre indicador");
+		new TextBox(crearIndicador).setWidth(265);
+		
+		new Label(mainPanel).setText("");
+		new Label(mainPanel).setText("Indicador");
+		new Label(mainPanel).setBackground(Color.LIGHT_GRAY).setForeground(Color.WHITE).setFontSize(12).setWidth(150);
+		
 		Panel operaciones = new Panel(mainPanel);
 		operaciones.setLayout(new HorizontalLayout());
-
+		
 		new Button(operaciones).setCaption("+").setWidth(55).setHeight(30);
 		new Button(operaciones).setCaption("-").setWidth(55).setHeight(30);
 		new Button(operaciones).setCaption("/").setWidth(55).setHeight(30);
 		new Button(operaciones).setCaption("*").setWidth(55).setHeight(30);
 		new Button(operaciones).setCaption("(").setWidth(55).setHeight(30);
 		new Button(operaciones).setCaption(")").setWidth(55).setHeight(30);
+		new Button(operaciones).setCaption("←").setWidth(55).setHeight(30);
 
-		new Label(mainPanel).setText("Indicador");
-		new Label(mainPanel).setBackground(Color.LIGHT_GRAY).setForeground(Color.WHITE).setFontSize(12).setWidth(150);
-
+		Panel constante = new Panel(mainPanel);
+		constante.setLayout(new HorizontalLayout());
+		
+		new Label(constante).setText("                        ");
+		new NumericField(constante).setWidth(60);
+		new Button(constante).setCaption("Agregar constante");
+		
 		new Button(mainPanel).setCaption("Guardar");
 
 	}
