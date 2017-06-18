d5606f85aa2f41c69eeeddb8d7cd93d661da4ba8
Manuel <jmanueltorrico_0593@hotmail.com>
2017-06-18 18:41:29 -0300

    Test de parser agregados

--- src/test/java/model/parser/ParserTest.java
+++ src/test/java/model/parser/ParserTest.java
@@ -1,48 +1,43 @@
 package model.parser;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import imports.ImportadorDeEmpresasCSV;
 import indicators.DataIndicador;
 import model.parser.objetosParser.*;
-import repositories.RepositorioCuentas;
-import repositories.RepositorioEmpresas;
+import readers.LectorCuentas;
+import readers.LectorIndicadores;
+import repositories.RepositorioIndicadores;
 
 public class ParserTest{
 	DataIndicador indicadorSimple;
 	DataIndicador indicadorA;
 	DataIndicador indicadorB;
 	DataIndicador indicadorC;
-	List<DataIndicador> indicadores;
 	public double resultado;
-	RepositorioCuentas mockedRepoCuentas = mock(RepositorioCuentas.class);
-	RepositorioEmpresas mockedRepoEmpresas = mock(RepositorioEmpresas.class);
+	public String resultadoString;
 	List<String> listaCuentas;
+	public ImportadorDeEmpresasCSV importadorCSV;
+	ExpresionLexer lexer;
 	
 	@Before
 	public void setUp(){
 		indicadorSimple = new DataIndicador("indicadorSimple", "15+35");
 		indicadorA = new DataIndicador("indicadorA", "ebitda + 100");
 		indicadorB = new DataIndicador("indicadorB", "fds * 10");
-		indicadorC = new DataIndicador("indicadorC", "indicador1 + fds");
+		indicadorC = new DataIndicador("indicadorC", "indicadorA + fds");
 		
-		indicadores = new LinkedList<>();
-		indicadores.add(indicadorA);
-		indicadores.add(indicadorB);
-		indicadores.add(indicadorC);
-		
-		listaCuentas = new LinkedList<>();
-		listaCuentas.add("ebitda");
-		listaCuentas.add("free cash flow");
-		listaCuentas.add("fds");
-		listaCuentas.add("operaciones continuas");
-		listaCuentas.add("operaciones discontinuas");
+		importadorCSV = new ImportadorDeEmpresasCSV("empresas.csv");
+		importadorCSV.importar();
+		new LectorCuentas("./cuentas.json").importar();
+		new LectorIndicadores().importar();
+		lexer = new ExpresionLexer();
 	}
 
 	@Test
@@ -62,7 +57,6 @@ public class ParserTest{
 	@Test
 	public void evaluarOperacion(){
 		//hago que el lexer cree y evalue segun la operacion 2*3+4
-		final ExpresionLexer lexer = new ExpresionLexer();
 		final ConstanteParser cons = new ConstanteParser("2");
 		final ConstanteParser cons1 = new ConstanteParser("3");
 		final ConstanteParser cons2 = new ConstanteParser("4");
@@ -78,7 +72,7 @@ public class ParserTest{
 	
 	@Test
 	public void evaluarOperacion2(){
-		final ExpresionLexer lexer = new ExpresionLexer();
+		//hago que el lexer cree y evalue segun la operacion ebitda+100 para sprite en periodo 2016
 		final ConstanteParser cons = new ConstanteParser("100");
 		final CuentaParser cuenta = new CuentaParser("ebitda");
 		lexer.expresiones.add(cuenta);
@@ -86,5 +80,34 @@ public class ParserTest{
 		lexer.operadores.add("+");
 		lexer.crearOperacion(lexer.expresiones);
 		assertEquals(lexer.expresiones.get(0).operar("sprite", "2016"),(double) 600,(double) 0);
+	}
+	
+	@Test
+	public void evaluarIndicador(){
+		assertEquals(indicadorSimple.evaluateEn("", ""),(double) 50,(double) 0);
+	}
+	
+	@Test
+	public void evaluarIndicadorA(){
+		RepositorioIndicadores.getInstance().agregar(indicadorA);
+		resultado = indicadorA.evaluateEn("coca cola", "2017");
+		assertEquals(resultado,(double) 1125.55,(double) 2);
+	}
+	
+	@Test
+	public void evaluarIndicadorB(){
+		RepositorioIndicadores.getInstance().agregar(indicadorB);
+		resultado = indicadorB.evaluateEn("sprite", "2016");
+		assertEquals(resultado,(double) 10000000,(double) 0);
+	}
+	
+	@Test
+	public void evaluarIndicadorC(){
+		RepositorioIndicadores.getInstance().agregar(indicadorA);
+		RepositorioIndicadores.getInstance().agregar(indicadorC);
+		resultado = indicadorC.evaluateEn("sprite", "2016");
+		DecimalFormat formato = new DecimalFormat("###############.############");
+		resultadoString = String.valueOf(formato.format(resultado));
+		assertEquals(resultadoString, "1000600");
 	}
 }
