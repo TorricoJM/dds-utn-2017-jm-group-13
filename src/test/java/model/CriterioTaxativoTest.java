a05f19bae926a09124b4b307d8c8df814401dcc0
Manuel <jmanueltorrico_0593@hotmail.com>
2017-07-21 17:21:33 -0300

    fixed test criterioTaxativoTest

--- src/test/java/model/CriterioTaxativoTest.java
+++ src/test/java/model/CriterioTaxativoTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
@@ -24,25 +25,29 @@ public class CriterioTaxativoTest {
 	public Indicador indicador1 = new DataIndicador("i1","ebitda*10");
 	public CriterioTaxativo taxativo = new CriterioTaxativo("tax1",OperadorComparacion.MAYOR,indicador1,new Normal(),500);
 	public List<Empresa> empresasEsperadas = new LinkedList<>();
+	public List<Empresa> empresasRepo = new LinkedList<>();
 	
 	@Before
 	public void setUp(){
 		importador.importar();
 		
 		RepositorioIndicadores.getInstance().agregar(indicador1);
+		empresasRepo.addAll(RepositorioEmpresas.getInstance().getListaEmpresas());
 	}
 	
 	@Test
 	public void taxativoIndicador1MayorA500() {
-		List<PeriodoFiscal> periodos = RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa1").getPeriodos();
+		List<String> periodos = RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa1").getPeriodos().stream().map(p->p.getPeriodo()).collect(Collectors.toList());
 		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa2"));
 		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa3"));
-		assertEquals(taxativo.evaluar(periodos),empresasEsperadas);
+		assertEquals(taxativo.evaluar(periodos,empresasRepo),empresasEsperadas);
 		
 	}
-	
+
 	@After
 	public void tearDown(){
 		RepositorioIndicadores.deleteInstance();
+		empresasEsperadas.clear();
+		empresasRepo.clear();
 	}
 }
