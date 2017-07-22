8215dd4552470e75e06242d783f2cede0c1d108b
avaromatias <avaromatias@gmail.com>
2017-07-22 16:41:54 -0300

    Agrego funcionalidad multicriterios

--- src/main/java/methodologies/DataMetodologia.java
+++ src/main/java/methodologies/DataMetodologia.java
@@ -1,10 +1,13 @@
 package methodologies;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import criterios.Criterio;
 import model.Empresa;
 
+import org.apache.commons.lang.StringUtils;
 import org.uqbar.commons.utils.Observable;
 
 @Observable
@@ -17,9 +20,18 @@ public class DataMetodologia {
 		this.setCriterios(criterios);
 	}
 	
-	public List<Empresa> aplicarMetodologia(List<Empresa> listaEmpresas, List<String> listaPeriodos) {
+	public List<Empresa> aplicarMetodologia(List<Empresa> listaEmpresas, List<String> listaPeriodos) {		
+		List<Empresa> empresasResultantes = new LinkedList<>();
 		
-		return criterios.get(0).evaluar(listaPeriodos, listaEmpresas);
+		for (int i = 0; i < this.criterios.size(); i++){
+		empresasResultantes.addAll(this.criterios.get(i).evaluar(listaPeriodos, listaEmpresas));
+		}
+		
+		return empresasResultantes.stream().filter(e -> this.cuantasVecesSeRepiteLaEmpresa(empresasResultantes, e) == this.criterios.size()).distinct().collect(Collectors.toList());
+	}
+	
+	public int cuantasVecesSeRepiteLaEmpresa(List<Empresa> listaEmpresas, Empresa empresa){
+		return StringUtils.countMatches(listaEmpresas.toString(), empresa.toString());
 	}
 
 	public String getNombre() {
