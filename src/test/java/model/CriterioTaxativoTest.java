6e261dc6266155264285fef240b1957fc9b36ad6
manuel <manuel@Manuel>
2017-08-06 16:35:28 -0300

    Se agregan tests.

--- src/test/java/model/CriterioTaxativoTest.java
+++ src/test/java/model/CriterioTaxativoTest.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import criterios.CriterioTaxativo;
 import criterios.OperadorComparacion;
 import criterios.modificador.Normal;
+import criterios.modificador.Promedio;
+import criterios.modificador.Sumatoria;
 import imports.ImportadorDeEmpresasCSV;
 import indicators.DataIndicador;
 import indicators.Indicador;
@@ -24,8 +26,11 @@ public class CriterioTaxativoTest {
 	public ImportadorDeEmpresasCSV importador = new ImportadorDeEmpresasCSV("empresasTest.csv");
 	public Indicador indicador1 = new DataIndicador("i1","ebitda*10");
 	public CriterioTaxativo taxativo = new CriterioTaxativo("tax1",OperadorComparacion.MAYOR,indicador1,new Normal(),500);
+	public CriterioTaxativo taxativo2 = new CriterioTaxativo("tax2",OperadorComparacion.MAYOR,indicador1,new Sumatoria(),2500);
+	public CriterioTaxativo taxativo3 = new CriterioTaxativo("tax3",OperadorComparacion.MENOR,indicador1,new Promedio(),750);
 	public List<Empresa> empresasEsperadas = new LinkedList<>();
 	public List<Empresa> empresasRepo = new LinkedList<>();
+	public List<String> periodos = new LinkedList<>();
 	
 	@Before
 	public void setUp(){
@@ -33,15 +38,26 @@ public class CriterioTaxativoTest {
 		importador.importar();
 		RepositorioIndicadores.getInstance().agregar(indicador1);
 		empresasRepo.addAll(RepositorioEmpresas.getInstance().getListaEmpresas());
+		periodos = RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa1").getPeriodos().stream().map(p->p.getPeriodo()).collect(Collectors.toList());
 	}
 	
 	@Test
 	public void taxativoIndicador1MayorA500() {
-		List<String> periodos = RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa1").getPeriodos().stream().map(p->p.getPeriodo()).collect(Collectors.toList());
 		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa2"));
 		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa3"));
 		assertEquals(taxativo.evaluar(periodos,empresasRepo),empresasEsperadas);
-		
+	}
+	
+	@Test
+	public void taxativoSumatoriaMayorA2500() {
+		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa3"));
+		assertEquals(taxativo2.evaluar(periodos, empresasRepo),empresasEsperadas);
+	}
+	
+	@Test
+	public void taxativoPromedioMenorA7500() {
+		empresasEsperadas.add(RepositorioEmpresas.getInstance().obtenerEmpresaDesdeNombre("empresa1"));
+		assertEquals(taxativo3.evaluar(periodos, empresasRepo),empresasEsperadas);
 	}
 
 	@After
