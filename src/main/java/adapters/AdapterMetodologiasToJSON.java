2bdc68413e1092eaa31670f8fd30e2bfdfb3f5ad
fconsoni <franco.consoni@gmail.com>
2017-08-16 06:09:41 -0300

    Creado export de metodologias

---
+++ src/main/java/adapters/AdapterMetodologiasToJSON.java
@@ -0,0 +1,28 @@
+package adapters;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.reflect.TypeToken;
+
+import methodologies.Metodologia;
+import repositories.RepositorioMetodologias;
+
+public class AdapterMetodologiasToJSON extends AdaptadorToJSON<List<Metodologia>> {
+
+	@Override
+	protected List<Metodologia> getContenido() {
+		return RepositorioMetodologias.getInstance().getListaMetodologiasForExport();
+	}
+
+	@Override
+	protected void setContenido(List<Metodologia> contenido) {
+		RepositorioMetodologias.getInstance().agregarTodo(contenido);
+	}
+
+	@Override
+	protected Type obtenerTypeToken() {
+		return new TypeToken<List<Metodologia>>(){}.getType();
+	}
+
+}
--- src/main/java/repositories/RepositorioMetodologias.java
+++ src/main/java/repositories/RepositorioMetodologias.java
@@ -2,6 +2,7 @@ package repositories;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import methodologies.Metodologia;
 import methodologies.MetodologiesBuilder;
@@ -9,27 +10,36 @@ import methodologies.MetodologiesBuilder;
 public class RepositorioMetodologias {
 
 	private static RepositorioMetodologias instance;
-	
+
 	public static RepositorioMetodologias getInstance() {
-		if(instance == null){
+		if (instance == null) {
 			instance = new RepositorioMetodologias();
 			instance.agregar(new MetodologiesBuilder().buildPredefWarrenBuffet().build());
 		}
-		
+
 		return instance;
 	}
-	
+
 	public static void deleteInstance() {
 		instance = null;
 	}
-	
+
 	private List<Metodologia> listaMetodologias = new LinkedList<>();
 
 	public List<Metodologia> getListaMetodologias() {
 		return listaMetodologias;
 	}
 
+	public List<Metodologia> getListaMetodologiasForExport() {
+		return listaMetodologias.stream().filter(met -> !met.getNombre().equals("Warren Buffet"))
+				.collect(Collectors.toList());
+	}
+
 	public void agregar(Metodologia metodologia) {
 		listaMetodologias.add(metodologia);
+	}
+
+	public void agregarTodo(List<Metodologia> unasMetodologias) {
+		listaMetodologias.addAll(unasMetodologias);
 	}
 }
