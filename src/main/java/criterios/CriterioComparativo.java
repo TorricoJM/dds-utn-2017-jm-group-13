ec7b44c8c026d83049f3409667f4d8198c1073a3
Alejo Perrone <lejaperrone@gmail.com>
2017-09-11 12:01:57 -0300

    Relaciones criterioComparativo, funciona para indicadores no prefedinidos porque no estan en la base todavia

--- src/main/java/criterios/CriterioComparativo.java
+++ src/main/java/criterios/CriterioComparativo.java
@@ -3,6 +3,16 @@ package criterios;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import org.uqbar.commons.utils.Observable;
 
 import criterios.modificador.Modificador;
@@ -10,18 +20,34 @@ import criterios.modificador.Sumatoria;
 import indicators.Indicador;
 import model.Empresa;
 
+@Entity
 @Observable
 public class CriterioComparativo implements Criterio {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long id;
+	
 	public String nombre;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
 	public Indicador indicador;
+	
+	@Enumerated(EnumType.STRING)
 	public OperadorComparacion operador;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
 	public Modificador modificador = new Sumatoria();
+	
+	
 
 	public CriterioComparativo(String nombre, OperadorComparacion operador, Indicador indicador) {
 		this.nombre = nombre;
 		this.operador = operador;
 		this.indicador = indicador;
+	}
+	
+	public CriterioComparativo(){
 	}
 
 	@Override
