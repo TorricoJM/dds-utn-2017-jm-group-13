cddf820713d00413d6438cbfff5546e8356b0aed
TorricoJM <jmanueltorrico.0593@gmail.com>
2017-10-17 09:58:44 -0300

    usuario con lista de indicadores y lista de metodologias

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,11 +1,21 @@
 package user;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import org.uqbar.commons.utils.Observable;
+
+import indicators.Indicador;
+import methodologies.Metodologia;
 
 @Observable
 @Entity
@@ -15,6 +25,12 @@ public class User {
 	private Long id;
 	private String nombre;
 	private String password;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "user_id")
+	private List<Indicador> indicadores = new LinkedList<>();
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "user_id")
+	private List<Metodologia> metodologias = new LinkedList<>();
 	
 	public User() {
 	}
@@ -35,5 +51,21 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Indicador> getIndicadores() {
+		return indicadores;
+	}
+
+	public void setIndicadores(List<Indicador> indicadores) {
+		this.indicadores = indicadores;
+	}
+
+	public List<Metodologia> getMetodologias() {
+		return metodologias;
+	}
+
+	public void setMetodologias(List<Metodologia> metodologias) {
+		this.metodologias = metodologias;
 	}
 }
